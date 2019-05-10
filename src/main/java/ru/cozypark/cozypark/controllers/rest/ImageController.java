15ebd1cbefe67d1c13b962f2d253beb3e0633c07
Sergey <rock-n-rolllll@bk.ru>
2019-05-10 19:23:43 +0300

    useless commit message

--- src/main/java/ru/cozypark/cozypark/controllers/rest/ImageController.java
+++ src/main/java/ru/cozypark/cozypark/controllers/rest/ImageController.java
@@ -19,33 +19,33 @@ public class ImageController {
         this.service = service;
     }
 
-    @GetMapping
-    public ResponseEntity<ByteArrayResource> resource(Long issueId) {
-//       return new ByteArrayResource(service.findById(issueId).getImageContent());
-
-        Issue issue = service.findById(issueId);
-
-        MediaType type = MediaType.ALL;
-        String[] tokens = issue.getImageName().split("\\.");
-        if (tokens[tokens.length - 1].toLowerCase().equals("png")) {
-            type = MediaType.IMAGE_PNG;
-        }
-
-        if (tokens[tokens.length - 1].toLowerCase().equals("jpeg")||tokens[tokens.length - 1].toLowerCase().equals("jpg")) {
-            type = MediaType.IMAGE_JPEG;
-        }
-
-        if (tokens[tokens.length - 1].toLowerCase().equals("gif")) {
-            type = MediaType.IMAGE_GIF;
-        }
-
-        return ResponseEntity.ok()
-                // Content-Disposition
-//                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + issue.getImageName())
-                // Content-Type
-                .contentType(type)
-                // Contet-Length
-                .contentLength(issue.getImageContent().length) //
-                .body(new ByteArrayResource(issue.getImageContent()));
-    }
+//    @GetMapping
+//    public ResponseEntity<ByteArrayResource> resource(Long issueId) {
+////       return new ByteArrayResource(service.findById(issueId).getImageContent());
+//
+//        Issue issue = service.findById(issueId);
+//
+//        MediaType type = MediaType.ALL;
+//        String[] tokens = issue.getImageName().split("\\.");
+//        if (tokens[tokens.length - 1].toLowerCase().equals("png")) {
+//            type = MediaType.IMAGE_PNG;
+//        }
+//
+//        if (tokens[tokens.length - 1].toLowerCase().equals("jpeg")||tokens[tokens.length - 1].toLowerCase().equals("jpg")) {
+//            type = MediaType.IMAGE_JPEG;
+//        }
+//
+//        if (tokens[tokens.length - 1].toLowerCase().equals("gif")) {
+//            type = MediaType.IMAGE_GIF;
+//        }
+//
+//        return ResponseEntity.ok()
+//                // Content-Disposition
+////                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=" + issue.getImageName())
+//                // Content-Type
+//                .contentType(type)
+//                // Contet-Length
+//                .contentLength(issue.getImageContent().length) //
+//                .body(new ByteArrayResource(issue.getImageContent()));
+//    }
 }
--- src/main/java/ru/cozypark/cozypark/service/IssueService.java
+++ src/main/java/ru/cozypark/cozypark/service/IssueService.java
@@ -1,10 +1,16 @@
 package ru.cozypark.cozypark.service;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.beans.BeanUtils;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.ResourceAccessException;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartFile;
 import ru.cozypark.cozypark.models.Issue;
 import ru.cozypark.cozypark.models.User;
@@ -13,12 +19,13 @@ import ru.cozypark.cozypark.payloads.IssueDtoResponse;
 import ru.cozypark.cozypark.repositories.IssueRepository;
 
 import java.io.IOException;
-import java.util.List;
+import java.util.Collections;
 import java.util.function.Function;
 
 @Service
 public class IssueService {
     private final IssueRepository repository;
+    private final RestTemplate restTemplate = new RestTemplate();
 
     public IssueService(IssueRepository repository) {
         this.repository = repository;
@@ -33,8 +40,15 @@ public class IssueService {
         issue.setUser(user);
 
         issue.setImageName(file.getOriginalFilename());
-        issue.setImageContent(file.getBytes());
 
+        MultiValueMap body = new LinkedMultiValueMap<>();
+        body.put("image", Collections.singletonList(new InputStreamResource(file.getInputStream())));
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
+        httpHeaders.set("Authorization", "Client-ID d2bebc8db7e9093");
+        HttpEntity requestEntity = new HttpEntity<>(body, httpHeaders);
+        ResponseEntity<ObjectNode> responseEntity= restTemplate.exchange("https://api.imgur.com/3/upload", HttpMethod.POST, requestEntity, ObjectNode.class);
+        issue.setImageContent(responseEntity.getBody().get("data").get("link").asText());
         repository.save(issue);
     }
 
@@ -57,7 +71,7 @@ public class IssueService {
             IssueDtoResponse res = new IssueDtoResponse();
 
             BeanUtils.copyProperties(issue, res);
-            res.setImageUrl("/img?issueId=" + issue.getId());
+            res.setImageUrl(issue.getImageContent());
             res.setUserId(issue.getUser().getId());
             return res;
         };
